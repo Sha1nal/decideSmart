964f81449aaaa9efa458bcc97a6598b7f9fd47e7
Shainal <shainalchetty@gmail.com>
2023-10-21 09:53:55 +0200

    Form templates built, stable

--- app/forms.py
+++ app/forms.py
@@ -1,15 +1,15 @@
 from flask_wtf import FlaskForm
-from wtforms import import StringField, PasswordField, BooleanField, SubmitField, EmailField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, EmailField
 from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
+    email = EmailField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Sign In')
 
-class SignUp(FlaskForm):
+class SignUpForm(FlaskForm):
     name = StringField('Name')
-    email = EmailField('Username', validators=[DataRequired()])
+    email = EmailField('Email', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
-    password_confirmation = PasswordField('Password', validators=[DataRequired()])
-    submit = SubmitField('Sign In')
+    password_confirmation = PasswordField('Confirm Password', validators=[DataRequired()])
+    submit = SubmitField('Sign Up')
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,22 @@
 from flask import render_template
 from app import app
+from app.forms import LoginForm, SignUpForm
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html')
+
+@app.route('/signup')
+def signup():
+    form = SignUpForm()
+    return render_template('signup.html', form=form)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', form=form)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
